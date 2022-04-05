5e6e431718a864bdf53da1bb5d21b292f423017c
unknown <rodrigopinto5050@gmail.com>
2022-04-05 08:45:28 -0300

    5/04

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/css/styles.css';
-import {Auth0Provider} from '@auth0/auth0-react'
 
 ReactDOM.render(
   <React.StrictMode>
